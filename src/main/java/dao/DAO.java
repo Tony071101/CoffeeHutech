572c930387d9a4b4ac7885676886d7a6eb468ec3
Tony071101 <tinpigboyapple07112001@gmail.com>
2022-04-21 23:21:40 +0700

    Update DAO.java

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -31,7 +31,7 @@ public class DAO {
             ps = conn.prepareStatement(query);
             rs = ps.executeQuery();
             while(rs.next()){
-                list.add(new Product(rs.getInt(1), rs.getString(2), rs.getString(5), rs.getDouble(3)));
+                list.add(new Product(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getDouble(4)));
             }
         }catch (Exception e){}
         
@@ -61,12 +61,12 @@ public class DAO {
             ps.setString(2, password);
             rs = ps.executeQuery();
             while(rs.next()){
-                return new Account(rs.getInt(1),rs.getString(2),rs.getString(3));
+                return new Account(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getInt(4));
             }
         }catch (Exception e) {}
         return null;
     }
-    public Account checkAccoutnExist(String username){
+    public Account checkAccountExist(String username){
         String query = "SELECT * FROM coffee_hutech.account\n" +
                 "where username = ?\n";
         try {
@@ -75,27 +75,129 @@ public class DAO {
             ps.setString(1, username);
             rs = ps.executeQuery();
             while(rs.next()){
-                return new Account(rs.getInt(1),rs.getString(2),rs.getString(3));
+                return new Account(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getInt(4));
             }
         }catch (Exception e) {}
         return null;
     }
-    public void Signup(String username, String password){
-        String query = "insert into coffee_hutech.account(username,password) values (?,?)";
-        try{
-            conn = new DBContext().getConnection();
+    public void signup(String username, String password) {
+        String query = "insert into coffee_hutech.account (username, password) values (?,?)";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
             ps = conn.prepareStatement(query);
             ps.setString(1, username);
             ps.setString(2, password);
             ps.executeUpdate();
-        }catch(Exception e){}
-    }
-    public static void main(String[] args) {
-        DAO dao = new DAO();
-        List<Product> list = dao.getAllProduct();
-        List<Category> listC = dao.getAllCategory();
-        for(Category o : listC) {
-            System.out.println(o);
+        } catch (Exception e) {
         }
     }
+    public Product getProduct(String txt) {
+        String query = "SELECT * FROM coffee_hutech.product WHERE id = ?";
+        List<Product> list = new ArrayList<>();
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, txt);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                1);
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+    public List<Product> getProductBySellID(int id) {
+        List<Product> list = new ArrayList<>();
+        String query = "SELECT * FROM coffee_hutech.product WHERE sell_ID = ?";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+    public Product getProductByID(String id) {
+        String query = "select * from coffee_hutech.product\n" +
+                        "where id = ?";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            ps.setString(1, id);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5));
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+    public void deleteProduct(String pid) {
+        String query = "delete from coffee_hutech.product\n"
+                + "where id = ?";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            ps.setString(1, pid);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
+
+    public void insertProduct(String name, String img, String price) {
+        String query = "insert into coffee_hutech.product (name, image, price)\n" +
+                        "values (?,?,?)";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            ps.setString(1, name);
+            ps.setString(2, img);
+            ps.setString(3, price);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
+
+    public void editProduct(String name, String image, String price, String pid) {
+        String query = "update coffee_hutech.product \n" +
+                        "set name = ?,\n" +
+                        "image = ?,\n" +
+                        "price = ?\n" +
+                        "where id = ?";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            ps.setString(1, name);
+            ps.setString(2, image);
+            ps.setString(3, price);
+            ps.setString(4, pid);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
+    //public static void main(String[] args) {
+       // DAO dao = new DAO();
+        //List<Product> list = dao.getAllProduct();
+        //List<Category> listC = dao.getAllCategory();
+        //for(Category o : listC) {
+            //System.out.println(o);
+        //}
+        //dao.signup("vinh", "222");
+    //}
 }
